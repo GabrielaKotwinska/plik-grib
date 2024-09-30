4354696ab46bc0c1000ebaeccba40d3c193b4d0b
GabrielaKotwinska <147843454+GabrielaKotwinska@users.noreply.github.com>
2024-09-30 19:24:49 +0200

    Add files via upload

--- grib.cpp
+++ grib.cpp
@@ -6,14 +6,14 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
-        cerr << "Użycie: " << argv[0] << " <nazwa_pliku_grib>" << std::endl;
+        cerr << "Użycie: " << argv[0] << " <nazwa_pliku_grib>" << endl;
         return 1;
     }
 
     const char* filename = argv[1];
     FILE* file = fopen(filename, "rb");
     if (!file) {
-        cerr << "Nie udało się otworzyć pliku: " << filename << std::endl;
+        cerr << "Nie udało się otworzyć pliku: " << filename << endl;
         return 1;
     }
 
@@ -21,15 +21,20 @@ int main(int argc, char* argv[]) {
     codes_handle* h = nullptr;
     long message_count = 0;
 
+    // Iterowanie przez wszystkie wiadomości GRIB w pliku
     while ((h = codes_handle_new_from_file(nullptr, file, PRODUCT_GRIB, &err)) != nullptr) {
         ++message_count;
 
         // Odczytaj ogólne informacje o wiadomości
-        long param, level, time_unit, step_range;
-        codes_get_long(h, "parameterNumber", &param);
-        codes_get_long(h, "level", &level);
-        codes_get_long(h, "timeUnit", &time_unit);
-        codes_get_long(h, "stepRange", &step_range);
+        long param = 0, level = 0, time_unit = 0, step_range = 0;
+        if ((err = codes_get_long(h, "parameterNumber", &param)) != CODES_SUCCESS ||
+            (err = codes_get_long(h, "level", &level)) != CODES_SUCCESS ||
+            (err = codes_get_long(h, "timeUnit", &time_unit)) != CODES_SUCCESS ||
+            (err = codes_get_long(h, "stepRange", &step_range)) != CODES_SUCCESS) {
+            cerr << "Błąd odczytu parametrów dla wiadomości nr: " << message_count << " (error: " << err << ")" << endl;
+            codes_handle_delete(h);
+            continue;  // przejdź do następnej wiadomości
+        }
 
         cout << "==========================\n";
         cout << "== General Message Info ==\n";
@@ -40,19 +45,24 @@ int main(int argc, char* argv[]) {
         cout << "Jednostka czasu: " << time_unit << "\n";
         cout << "Zakres kroku: " << step_range << "\n";
 
-        // Odczyt współrzędnych geograficznych 
-        double lat, lon;
-        codes_get_double(h, "latitudeOfFirstGridPointInDegrees", &lat);
-        codes_get_double(h, "longitudeOfFirstGridPointInDegrees", &lon);
-        cout << "Początkowa szerokość geograficzna: " << lat << "°\n";
-        cout << "Początkowa długość geograficzna: " << lon << "°\n";
+        // Odczyt współrzędnych geograficznych
+        double lat = 0.0, lon = 0.0;
+        if ((err = codes_get_double(h, "latitudeOfFirstGridPointInDegrees", &lat)) != CODES_SUCCESS ||
+            (err = codes_get_double(h, "longitudeOfFirstGridPointInDegrees", &lon)) != CODES_SUCCESS) {
+            cerr << "Błąd odczytu współrzędnych geograficznych (error: " << err << ")" << endl;
+        }
+        else {
+            cout << "Początkowa szerokość geograficzna: " << lat << "°\n";
+            cout << "Początkowa długość geograficzna: " << lon << "°\n";
+        }
 
-        // Zwalnianie zasobów
+        // Zwalnianie zasobów dla tej wiadomości
         codes_handle_delete(h);
     }
 
-    if (err != CODES_SUCCESS) {
-        std::cerr << "Błąd podczas odczytywania wiadomości GRIB." << std::endl;
+    // Sprawdzenie, czy zakończenie było spowodowane błędem
+    if (err != CODES_SUCCESS && err != CODES_END_OF_FILE) {
+        cerr << "Błąd podczas odczytywania wiadomości GRIB. Error code: " << err << endl;
     }
 
     fclose(file);
